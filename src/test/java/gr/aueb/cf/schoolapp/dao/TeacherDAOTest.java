a0330578f3d0eac019221efa34105cba481c140b
a8ana <a8anassis@gmail.com>
2023-07-17 21:02:49 +0300

    v0.2

---
+++ src/test/java/gr/aueb/cf/schoolapp/dao/TeacherDAOTest.java
@@ -0,0 +1,72 @@
+package gr.aueb.cf.schoolapp.dao;
+
+import gr.aueb.cf.schoolapp.dao.dbutil.DBHelper;
+import gr.aueb.cf.schoolapp.dao.exceptions.TeacherDAOException;
+import gr.aueb.cf.schoolapp.model.Teacher;
+import gr.aueb.cf.schoolapp.validator.TeacherValidator;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TeacherDAOTest {
+
+    private static ITeacherDAO teacherDAO;
+
+    @BeforeAll
+    public static void setupClass() throws SQLException {
+        teacherDAO = new TeacherDAOImpl();
+        DBHelper.eraseData();
+    }
+
+    @BeforeEach
+    void setUp() throws TeacherDAOException {
+        createDummyTeachers();
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        DBHelper.eraseData();
+    }
+
+
+    @Test
+    void persistAndGetTeacher() throws TeacherDAOException {
+        Teacher teacher = new Teacher();
+        teacher.setFirstname("Bob");
+        teacher.setLastname("Marley");
+        teacherDAO.insert(teacher);
+
+        List<Teacher> teachers = teacherDAO.getByLastname("Marley");
+        assertEquals(1, teachers.size());
+    }
+
+
+    public static void createDummyTeachers() throws TeacherDAOException {
+        Teacher teacher = new Teacher();
+        teacher.setFirstname("Athanassios");
+        teacher.setLastname("Androutsos");
+        teacherDAO.insert(teacher);
+
+        teacher = new Teacher();
+        teacher.setFirstname("Anna");
+        teacher.setLastname("Kefala");
+        teacherDAO.insert(teacher);
+
+        teacher = new Teacher();
+        teacher.setFirstname("Makis");
+        teacher.setLastname("kapetis");
+        teacherDAO.insert(teacher);
+
+        teacher = new Teacher();
+        teacher.setFirstname("John");
+        teacher.setLastname("Papa");
+        teacherDAO.insert(teacher);
+    }
+
+}
